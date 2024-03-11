feac92975c514e50cc81bd585c2b92bb035853c7
Sérgio Gonçalves <127428465+ISergioMiguelI@users.noreply.github.com>
2024-03-11 11:24:52 +0000

    update

---
+++ 2.py
@@ -0,0 +1,19 @@
+a = 5
+
+b = 3
+
+soma = a + b
+
+print("A soma de ", a "e", b ,"é igual a ",soma)
+
+subtracao = a - b
+
+print("A subtração de ", a "e", b ,"é igual a ",subtracao)
+
+multiplicacao = a * b
+
+print("A multiplicação de ", a "e", b ,"é igual a ",multiplicacao)
+
+divisao = a / b
+
+print("A divisão de ", a "e", b ,"é igual a ",divisao)
